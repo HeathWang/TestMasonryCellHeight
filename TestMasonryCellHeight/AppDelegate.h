b6dc7d66c4a6b30f506299f1d37a7eda8ae9e18b
Heath <heath@HeathdeMacBook-Pro.local>
2015-05-21 17:49:14 +0800

    Initial Commit

---
+++ TestMasonryCellHeight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMasonryCellHeight
+//
+//  Created by Heath on 15/5/21.
+//  Copyright (c) 2015年 Heath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestMasonryCellHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMasonryCellHeight
+//
+//  Created by Heath on 15/5/21.
+//  Copyright (c) 2015年 Heath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
